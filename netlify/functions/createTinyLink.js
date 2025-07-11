4758102984dbf34c48c41d79bdeeeca5f2496940
spikest3r <oleg0sher@outlook.com>
2025-07-11 14:46:40 +0300

    testing rev. 01

---
+++ netlify/functions/createTinyLink.js
@@ -0,0 +1,51 @@
+import { neon } from '@netlify/neon';
+const sql = neon();
+
+// generator for tiny link. 
+function randomString(length = 6) {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+exports.handler = async (event) => {
+    const ogLink = event.queryStringParameters.link;
+    try {
+        let tiny;
+        let maxTries = 10;
+        let found = false;
+        // check if generated tiny link is unique, give up on 10 attempts
+        for(let i = 0; i < maxTries; i++) {
+            tiny = randomString();
+            const existing = await sql`SELECT 1 FROM urls WHERE short_url = ${tiny}`;
+            if(existing.length === 0) {
+                found = true;
+                break;
+            }
+        }
+        if(found) {
+            await sql`INSERT INTO urls(original_url, short_url) VALUES (${ogLink}, ${tiny})` // insert link into db (store only id)
+            return {
+                statusCode: 200,
+                body: JSON.stringify(tiny), // stringify the result
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+        } else {
+            throw new Error("Couldn't generate tiny link"); // throw error so try/catch can return error to client
+        }
+    } catch (error) {
+        console.error('DB error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'DB query failed' }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    }
+};
---
+++ netlify/functions/redirector.js
@@ -0,0 +1,20 @@
+exports.handler = async (event) => {
+    const path = event.path.replace("/.netlify/functions/redirector/", "")
+    console.log(path)
+    const query = await sql`SELECT original_url FROM urls WHERE short_url = ${path}`;
+    const ogLink = query[0]?.original_url;
+
+    if(ogLink) {
+        return {
+            statusCode: 302,
+            headers: {
+                Location: ogLink
+            }
+        }
+    } else {
+        return {
+            statusCode: 404,
+            body: '<h1>=( Not found </h1>'
+        }
+    }
+}
